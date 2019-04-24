d900d025dfd1a90dbabebe2122e61c38ac1e05d9
Aliya Chambless <aliya@brightday.io>
2019-04-24 15:35:46 -0400

    syntax comments

--- webcam_test.py
+++ webcam_test.py
@@ -10,6 +10,18 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # faces = faceCascade.detectMultiScale(
+    #     gray,
+    #     scaleFactor=1.1,
+    #     minNeighbors=5,
+    #     minSize=(30, 30),
+    #     flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    # )
+
+    # # Draw a rectangle around the faces
+    # for (x, y, w, h) in faces:
+    #     cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
     # Display the resulting frame
     cv2.imshow('Video', gray)
 
