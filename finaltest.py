ef9d20686102b4a6b04b96d848717967345be145
Aliya Chambless <aliya@brightday.io>
2019-04-28 21:19:17 -0400

    working model

--- finaltest.py
+++ finaltest.py
@@ -2,59 +2,65 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
-num_imgs = 50000
+import os
 
-img_size = 16
-min_rect_size = 3
-max_rect_size = 8
-num_objects = 2
-
-bboxes = np.zeros((num_imgs, num_objects, 4))
-imgs = np.zeros((num_imgs, img_size, img_size))
-shapes = np.zeros((num_imgs, num_objects, 1))
-
-for i_img in range(num_imgs):
-    for i_object in range(num_objects):
-        if np.random.choice([True, False]):
-            width, height = np.random.randint(min_rect_size, max_rect_size, size=2)
-            x = np.random.randint(0, img_size - width)
-            y = np.random.randint(0, img_size - height)
-            imgs[i_img, x:x+width, y:y+height] = 1.
-            bboxes[i_img, i_object] = [x, y, width, height]
-            shapes[i_img, i_object] = [0]
-        else:
-            size = np.random.randint(min_rect_size, max_rect_size)
-            x, y = np.random.randint(0, img_size - size, size=2)
-            mask = np.tril_indices(size)
-            imgs[i_img, x + mask[0], y + mask[1]] = 1.
-            bboxes[i_img, i_object] = [x, y, size, size]
-            shapes[i_img, i_object] = [1]
-            
-imgs.shape, bboxes.shape
-
-i = 0
-# TODO: Why does the array have to be transposed?
-plt.imshow(imgs[i].T, cmap='Greys', interpolation='none', origin='lower', extent=[0, img_size, 0, img_size])
-for bbox, shape in zip(bboxes[i], shapes[i]):
-    plt.gca().add_patch(matplotlib.patches.Rectangle((bbox[0], bbox[1]), bbox[2], bbox[3], ec='r' if shape[0] == 0 else 'y', fc='none'))
-
-X = (imgs.reshape(num_imgs, -1) - np.mean(imgs)) / np.std(imgs)
-X.shape, np.mean(X), np.std(X)
-
-y = np.concatenate([bboxes / img_size, shapes], axis=-1).reshape(num_imgs, -1)
-y.shape
-
-i = int(0.8 * num_imgs)
-train_X = X[:i]
-test_X = X[i:]
-train_y = y[:i]
-test_y = y[i:]
-test_imgs = imgs[i:]
-test_bboxes = bboxes[i:]
+import numpy as np
+from pandas.io.parsers import read_csv
+from sklearn.utils import shuffle
 
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
+
+
+FTRAIN = './training.csv'
+FTEST = './test.csv'
+
+
+def load(test=False, cols=None):
+    """Loads data from FTEST if *test* is True, otherwise from FTRAIN.
+    Pass a list of *cols* if you're only interested in a subset of the
+    target columns.
+    """
+    fname = FTEST if test else FTRAIN
+    df = read_csv(os.path.expanduser(fname))  # load pandas dataframe
+
+    # The Image column has pixel values separated by space; convert
+    # the values to numpy arrays:
+    df['Image'] = df['Image'].apply(lambda im: np.fromstring(im, sep=' '))
+
+    if cols:  # get a subset of columns
+        df = df[list(cols) + ['Image']]
+
+    print(df.count())  # prints the number of values for each column
+    df = df.dropna()  # drop all rows that have missing values in them
+
+    X = np.vstack(df['Image'].values) / 255.  # scale pixel values to [0, 1]
+    X = X.astype(np.float32)
+
+    if not test:  # only FTRAIN has any target columns
+        y = df[df.columns[:-1]].values
+        y = (y - 48) / 48  # scale target coordinates to [-1, 1]
+        X, y = shuffle(X, y, random_state=42)  # shuffle train data
+        y = y.astype(np.float32)
+    else:
+        y = None
+
+    return X, y
+
+
+train_X, train_y = load()
+print("X.shape == {}; X.min == {:.3f}; X.max == {:.3f}".format(
+    train_X.shape, train_X.min(), train_X.max()))
+print("y.shape == {}; y.min == {:.3f}; y.max == {:.3f}".format(
+    train_y.shape, train_y.min(), train_y.max()))
+
+num_objects = 2
+
+X = train_X
+
+y = train_y
+
 model = Sequential([
         Dense(256, input_dim=X.shape[-1]), 
         Activation('relu'), 
@@ -63,8 +69,8 @@ model = Sequential([
     ])
 model.compile('adadelta', 'mse')
 
-# Flip bboxes during training.
-# Note: The validation loss is always quite big here because we don't flip the bounding boxes for the validation data. 
+# # Flip bboxes during training.
+# # Note: The validation loss is always quite big here because we don't flip the bounding boxes for the validation data. 
 def IOU(bbox1, bbox2):
     '''Calculate overlap between two bounding boxes [x, y, w, h] as the area of intersection over the area of unity'''
     x1, y1, w1, h1 = bbox1[0], bbox1[1], bbox1[2], bbox1[3]  # TODO: Check if its more performant if tensor elements are accessed directly below.
@@ -84,101 +90,28 @@ def dist(bbox1, bbox2):
     return np.sqrt(np.sum(np.square(bbox1[:2] - bbox2[:2])))
 
 num_epochs_flipping = 50
-num_epochs_no_flipping = 0  # has no significant effect
-flipped_train_y = np.array(train_y)
-flipped = np.zeros((len(flipped_train_y), num_epochs_flipping + num_epochs_no_flipping))
-ious_epoch = np.zeros((len(flipped_train_y), num_epochs_flipping + num_epochs_no_flipping))
-dists_epoch = np.zeros((len(flipped_train_y), num_epochs_flipping + num_epochs_no_flipping))
-mses_epoch = np.zeros((len(flipped_train_y), num_epochs_flipping + num_epochs_no_flipping))
 
 # TODO: Calculate ious directly for all samples (using slices of the array pred_y for x, y, w, h).
-for epoch in range(num_epochs_flipping):
-    print('Epoch', epoch)
-    model.fit(train_X, flipped_train_y, nb_epoch=1, validation_data=(test_X, test_y), verbose=2)
-    pred_y = model.predict(train_X)
+test_X, test_y = load(test=True)
 
-    for sample, (pred, exp) in enumerate(zip(pred_y, flipped_train_y)):
-        
-        # TODO: Make this simpler.
-        pred = pred.reshape(num_objects, -1)
-        exp = exp.reshape(num_objects, -1)
-        
-        pred_bboxes = pred[:, :4]
-        exp_bboxes = exp[:, :4]
-        
-        # TODO: Try flipping array and see if results differ.
-        ious = np.zeros((num_objects, num_objects))
-        dists = np.zeros((num_objects, num_objects))
-        mses = np.zeros((num_objects, num_objects))
-        for i, exp_bbox in enumerate(exp_bboxes):
-            for j, pred_bbox in enumerate(pred_bboxes):
-                ious[i, j] = IOU(exp_bbox, pred_bbox)
-                dists[i, j] = dist(exp_bbox, pred_bbox)
-                mses[i, j] = np.mean(np.square(exp_bbox - pred_bbox))
-                
-        new_order = np.zeros(num_objects, dtype=int)
-        
-        for i in range(num_objects):
-            # Find pred and exp bbox with maximum iou and assign them to each other (i.e. switch the positions of the exp bboxes in y).
-            ind_exp_bbox, ind_pred_bbox = np.unravel_index(mses.argmin(), mses.shape)
-            ious_epoch[sample, epoch] += ious[ind_exp_bbox, ind_pred_bbox]
-            dists_epoch[sample, epoch] += dists[ind_exp_bbox, ind_pred_bbox]
-            mses_epoch[sample, epoch] += mses[ind_exp_bbox, ind_pred_bbox]
-            mses[ind_exp_bbox] = 1000000#-1  # set iou of assigned bboxes to -1, so they don't get assigned again
-            mses[:, ind_pred_bbox] = 10000000#-1
-            new_order[ind_pred_bbox] = ind_exp_bbox
-        
-        flipped_train_y[sample] = exp[new_order].flatten()
-        
-        flipped[sample, epoch] = 1. - np.mean(new_order == np.arange(num_objects, dtype=int))#np.array_equal(new_order, np.arange(num_objects, dtype=int))  # TODO: Change this to reflect the number of flips.
-        ious_epoch[sample, epoch] /= num_objects
-        dists_epoch[sample, epoch] /= num_objects
-        mses_epoch[sample, epoch] /= num_objects
-        
-            
-    print('Flipped {} % of all elements'.format(np.mean(flipped[:, epoch]) * 100.))
-    print('Mean IOU: {}'.format(np.mean(ious_epoch[:, epoch])))
-    print('Mean dist: {}'.format(np.mean(dists_epoch[:, epoch])))
-    print('Mean mse: {}'.format(np.mean(mses_epoch[:, epoch])))
-    # print
+model.fit(train_X, train_y, 
+          batch_size=32, nb_epoch=15, verbose=2)
 
-plt.pcolor(flipped[:1000], cmap='Greys', vmax=1.)
-# plt.axvline(num_epochs_flipping, c='r')
-plt.xlabel('Epoch')
-plt.ylabel('Training sample')
+#display matches
 
+def plot_sample(x, y, axis):
+    img = x.reshape(96, 96)
+    axis.imshow(img, cmap='gray')
+    axis.scatter(y[0::2] * 48 + 48, y[1::2] * 48 + 48, marker='x', s=10)
 
-mean_ious_epoch = np.mean(ious_epoch, axis=0)
-mean_dists_epoch = np.mean(dists_epoch, axis=0)
-mean_mses_epoch = np.mean(mses_epoch, axis=0)
-plt.plot(mean_ious_epoch, label='Mean IOU')  # between predicted and assigned true bboxes
-plt.plot(mean_dists_epoch, label='Mean distance')  # relative to image size
-plt.plot(mean_mses_epoch, label='Mean mse')  # relative to image size
-plt.annotate(np.round(np.max(mean_ious_epoch), 3), (len(mean_ious_epoch)-1, mean_ious_epoch[-1]+0.03), horizontalalignment='right', color='b')
-plt.annotate(np.round(np.min(mean_dists_epoch), 3), (len(mean_dists_epoch)-1, mean_dists_epoch[-1]+0.03), horizontalalignment='right', color='g')
-plt.annotate(np.round(np.min(mean_mses_epoch), 3), (len(mean_mses_epoch)-1, mean_mses_epoch[-1]+0.03), horizontalalignment='right', color='r')
-# plt.axvline(num_epochs_flipping, c='r')
-plt.xlabel('Epoch')
-plt.legend()
-plt.ylim(0, 1)
+X, _ = load(test=True)
+y_pred = model.predict(X)
 
-# plt.savefig('plots/bw-multiple-rectangles-mse.png', dpi=300)
+fig = plt.figure(figsize=(6, 6))
+fig.subplots_adjust(
+    left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
 
-pred_y = model.predict(test_X)
-pred_y = pred_y.reshape(len(pred_y), num_objects, -1)
-pred_bboxes = pred_y[..., :4] * img_size
-pred_shapes = pred_y[..., 4:5]
-pred_bboxes.shape, pred_shapes.shape
-
-
-plt.figure(figsize=(16, 8))
-for i_subplot in range(1, 9):
-    plt.subplot(2, 4, i_subplot)
-    i = np.random.randint(len(test_X))
-    plt.imshow(test_imgs[i].T, cmap='Greys', interpolation='none', origin='lower', extent=[0, img_size, 0, img_size])
-    for pred_bbox, exp_bbox, pred_shape in zip(pred_bboxes[i], test_bboxes[i], pred_shapes[i]):
-        plt.gca().add_patch(matplotlib.patches.Rectangle((pred_bbox[0], pred_bbox[1]), pred_bbox[2], pred_bbox[3], ec='r' if pred_shape[0] <= 0.5 else 'y', fc='none'))
-        # TODO: Calculate max IOU with all expected bounding boxes.
-#         plt.annotate('IOU: {:.2f}'.format(IOU(pred_bbox, exp_bbox)), (pred_bbox[0], pred_bbox[1]+pred_bbox[3]+0.4), color='r')
-
+for i in range(16):
+    ax = fig.add_subplot(4, 4, i + 1, xticks=[], yticks=[])
+    plot_sample(X[i], y_pred[i], ax)
 plt.savefig('/home/aliyac1999/multi-predict.png', dpi=300)
