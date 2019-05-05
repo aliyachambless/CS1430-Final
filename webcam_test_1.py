342053cdb3eb23c33aae3725ce2016dae74664e0
vxu4 <vixixu68@gmail.com>
2019-05-05 19:42:41 -0400

    trying to integrate mapping

--- webcam_test_1.py
+++ webcam_test_1.py
@@ -10,6 +10,7 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow import keras
+from image_distortion.matchup import matchup as MU
 # import Image
 
 
@@ -19,6 +20,12 @@ video_capture = cv2.VideoCapture(0)
 if_not_first = False
 face_cascade = cv2.CascadeClassifier('/Users/victoriaxu/Documents/CS/opencv-4.0.0/data/haarcascades/haarcascade_frontalface_default.xml')
 model = keras.models.load_model("model2.h5")
+filter_image = cv2.cvtColor(cv2.imread('/Users/victoriaxu/Downloads/mockup.png'), cv2.COLOR_BGR2GRAY)
+filter_dict = {}
+filter_dict['left_eye'] = [25,27]
+filter_dict['right_eye'] = [25,67]
+filter_dict['nose'] = [45,47]
+filter_dict['bottom'] = [74,47]
 
 
 while True:
@@ -28,6 +35,7 @@ while True:
     #replicating input image to crop and feed into predictor 
     img_crop = gray 
     width, height = gray.shape
+
     if (if_not_first) :
         #gets rectangle of face
         faces = face_cascade.detectMultiScale(img_crop, 1.3, 5)
@@ -46,17 +54,55 @@ while True:
             #predicting with model
             face_info = model.predict(X)
 
+
             #defining plot
             def plot (fig) :
-                #rescaling face_info coords to 96x96 square
                 fig = fig * 48 + 48
+
+                face_dict = {}
+                face_dict['right_eye'] = [fig[0][1],fig[0][0]]
+                # cv2.circle(img_crop, (fig[0][0],fig[0][1]), 3, (255,0,0))
+                # print(str(fig[0][0]) + " " + str(fig[0][1]))
+                face_dict['left_eye'] = [fig[0][3], fig[0][2]]
+                # cv2.circle(img_crop, (fig[0][2], fig[0][3]), 3, (255,0,0))
+                # print(str(fig[0][2]) + " " + str(fig[0][3]))
+
+                face_dict['nose'] = [fig[0][21], fig[0][20]]
+                # cv2.circle(img_crop, (fig[0][20], fig[0][21]), 3, (255,0,0))
+                # print(str(fig[0][0]) + " " + str(fig[0][1]))
+
+                face_dict['bottom'] = [fig[0][29],fig[0,28]]
+                # cv2.circle(img_crop, (fig[0][28],fig[0,29]), 3, (255,0,0))
+                # print(str(fig[0][0]) + " " + str(fig[0][1]))
+
+                # cv2.circle(gray, (int((fig[0][i] * scaleX) + x), int((fig[0][i + 1] * scaleY) + y)), 3, (255,0,0))
+                # img_crop[:][:][0] = MU(img_crop[:][:][0], face_dict, filter_image, filter_dict)
+                # print(img_crop[:][:][0].shape)
+                # img_crop[:][:][1] = MU(img_crop[:][:][1], face_dict, filter_image, filter_dict)
+                # img_crop[:][:][2] = MU(img_crop[:][:][2], face_dict, filter_image, filter_dict)
+                # final = img_crop
+                final = MU(img_crop, face_dict, filter_image, filter_dict)
+
+                # final = np.array([MU(img_crop[:][0][:], face_dict, filter_image, filter_dict), MU(img_crop[:][1][:], face_dict, filter_image, filter_dict), MU(img_crop[:][2][:], face_dict, filter_image, filter_dict)])
+                print("BEGINNNIG ")
+                print(img_crop.shape)
+                print(face_dict)
+                print(filter_image.shape)
+                print(filter_dict)
+                print(final.shape)
+                #rescaling face_info coords to 96x96 square
                 i = 0
                 #rescaling to cropped rectangle shape, then shifting by x and y to map to right place on gray
                 while i < 29 :
-                    cv2.circle(gray, (int((fig[0][i] * scaleX) + x), int((fig[0][i + 1] * scaleY) + y)), 3, (255,0,0))
+                    # cv2.circle(gray, (int((fig[0][i] * scaleX) + x), int((fig[0][i + 1] * scaleY) + y)), 3, (255,0,0))
+                    # print("DIMENSIONS "+ str(gray.shape))
+                    # print("WHAT IS "+ str((int((fig[0][i] * scaleX) + x), int((fig[0][i + 1] * scaleY) + y))))
+                    fig[0][i] = int((fig[0][i] * scaleX) + x)
+                    fig[0][i + 1] = int((fig[0][i + 1] * scaleY) + y)
                     # print("HIT")
-                    i = i + 1
-                return gray
+                    i = i + 2
+
+                return final
 
             # Display the resulting frame
             cv2.imshow('Video', plot(face_info))
